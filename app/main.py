302fdfdc71d25d56ac8edabf34f2a96787a22977
Mia-acton <150271533+Mia-acton@users.noreply.github.com>
2026-09-18 11:01:07 +0000

    Added route for adding user called add_user

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException, status
-from app.schemas import UserCreate
+from app.schemas import userCreate
 
 app = FastAPI(title="Lab1 - FastAPI User Api")
 
-users: list[UserCreate] = []
+users: list[userCreate] = []
 @app.get("/health")
 def health():
     return {"status": "ok"}
@@ -14,7 +14,11 @@ def hello():
     return {"message": "Hello World"}
 
 @app.post("/api/users", status_code=status.HTTP_201_CREATED)
-def add_user(new_user: UserCreate):
-    users.append()(new_user)
+def add_user(new_user: userCreate):
+    for existing_user in users:
+        if existing_user.userid == new_user.userid:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail="A user with this id already exists")
+    users.append(new_user)
     return new_user
 
