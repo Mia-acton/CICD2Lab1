fff6dd3c92bc45d90d7a7b23c182d060239bbfb2
Mia-acton <150271533+Mia-acton@users.noreply.github.com>
2026-09-18 10:38:25 +0000

    Typo with function name

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ app = FastAPI(title="Lab1 - FastAPI User Api")
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello World"}
+
